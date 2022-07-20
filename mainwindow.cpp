5620bb8d69c84491f2fecba9891d9cae241f3b76
Jonathan PEREIRA <j.pereira@autom-elec.com>
2022-07-20 21:57:50 +0200

    Début installeur

---
+++ mainwindow.cpp
@@ -0,0 +1,45 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+
+    //Init Variable//
+    seconds = 0;
+
+    ui->setupUi(this);
+
+
+    timer = new QTimer(this);
+    timer->start(1000);
+
+    connect(timer, &QTimer::timeout, this, &MainWindow::ElapsedTime);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::ElapsedTime()
+{
+    seconds++;
+    if(seconds == 60) {
+        seconds = 0;
+        minutes += 1;
+    }
+    ui->timer->setText(QString::number(minutes).rightJustified(2, '0') + ":" + QString::number(seconds).rightJustified(2,'0'));
+
+    MoveProgressBar();
+}
+
+void MainWindow::MoveProgressBar()
+{
+    ui->progressBar->setRange(0,100);
+    ui->progressBar->setValue(seconds);
+}
